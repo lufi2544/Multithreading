feca04a893969e7f528bbefd2438408c5d9c313f
Juanes Rayo Work <lufi2544@gmail.com>
2025-07-10 12:29:11 +0200

    Added mutex tests

--- source/main.cpp
+++ source/main.cpp
@@ -18,10 +18,15 @@ int main(int num, char** args)
 	//ids_test();
 	//detach_test();
 	
-	SCRATCH();	
-	string_t string = STRING_V(temp_arena, "abcdef");
+	/*SCRATCH();	
+	string_t string_1 = STRING_V(temp_arena, "abcdef");
+	string_t string_2 = STRING_V(temp_arena, "xyz");
+	string_t string_3 = STRING_V(temp_arena, "oooo");
 	
-	string_test(string);
+	print_string_test(string_1, string_2, string_3);
+*/
+	
+	lock_test();
 	
 	return 0;
 }
--- source/threading_tests.cpp
+++ source/threading_tests.cpp
@@ -61,28 +61,63 @@ detach_test()
 	printf("Detach_Test_Finished \n");
 }
 
-
-void print_string_(string_t string)
-{
-	
-}
-
 void string_test(string_t string)
 {
-	for(int i = 0; i < string.size; ++i)
+	for(int j = 0; j < 5; ++j)
 	{
-		u8 letter = (u8)(STRING_CONTENT(string))[i];
-		if(i == string.size - 1)
+		for(int i = 0; i < string.size; ++i)
 		{
-		printf("%c \n", letter);
-			
-		}
-		else
-		{
-		printf("%c", letter);
-			
+			u8 letter = (u8)(STRING_CONTENT(string))[i];
+			if(i == string.size - 1)
+			{
+				printf("%c \n", letter);
+				
+			}
+			else
+			{
+				printf("%c", letter);
+				
+			}
 		}
 	}
+}
+
+critical_section_t string_critical;
+
+void
+string_test_mutex(string_t string)
+{
+	for(int j = 0; j < 5; ++j)
+	{
+		for(int i = 0; i < string.size; ++i)
+		{
+			string_critical.lock();
+			u8 letter = (u8)(STRING_CONTENT(string))[i];
+			if(i == string.size - 1)
+			{
+				printf("%c \n", letter);
+				
+			}
+			else
+			{
+				printf("%c", letter);
+				
+			}
+			string_critical.unlock();
+		}
+	}
+}
+
+void print_string_test(string_t _a, string_t _b, string_t _c)
+{
+	thread_t t1(string_test_mutex, _a);
+	thread_t t2(string_test_mutex, _b);
+	thread_t t3(string_test_mutex, _c);
+	
+	t1.join();
+	t2.join();
+	t3.join();
+	
 }
 
 global_f void 
@@ -101,4 +136,40 @@ access_test()
 	
 	printf("Final Counter %i \n", g_counter.load());
 	
+}
+
+critical_section_t global_critical;
+
+void
+task1()
+{
+	global_critical.lock();
+	printf("task 1 -> locking. \n");
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	global_critical.unlock();
+	printf("task 1 -> unlocking. \n");
+}
+
+void task2()
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	
+	
+	while(!global_critical.try_lock())
+	{
+		printf("task2 -> could not lock, sleeping for 100ms \n");
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+	
+	printf("task2 -> unlocking \n.");	
+}
+
+
+void lock_test()
+{
+	thread_t t1(task1);
+	thread_t t2(task2);
+	
+	t1.join();
+	t2.join();
 }
